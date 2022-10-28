117b39dbd95e74025cd7beb01d5439976ed9f369
Erick-Toala <94879384+Erick-Toala@users.noreply.github.com>
2022-10-28 09:48:35 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+let cerrar =document.querySelectorAll(".close")[0];
+let abrir =document.querySelectorAll(".cta")[0];
+let modal =document.querySelectorAll(".modal")[0];
+let modalContainer =document.querySelectorAll(".modal-container")[0];
+
+abrir.addEventListener("click", function(e){
+    e.preventDefault();
+    modalContainer.style.opacity="1";
+    modalContainer.style.visibility="visible";
+    modal.classList.toggle("modal-close");
+});
+
+cerrar.addEventListener("click", function(){
+    modal.classList.toggle("modal-close");
+    setTimeout(function(){
+        modalContainer.style.opacity="0";
+        modalContainer.style.visibility="hidden";
+    },750)
+});
+
+window.addEventListener("click",function(e){
+    console.log(e.target)
+    if(e.target==modalContainer){
+        modal.classList.toggle("modal-close");
+        setTimeout(function(){
+            modalContainer.style.opacity="0";
+            modalContainer.style.visibility="hidden";
+        },750)
+    }
+})
